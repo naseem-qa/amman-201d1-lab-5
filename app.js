e17e0cc1aab170bebf8bf4aee350ad92d15f734a
naseem <izzatnaseem30@gmail.com>
2019-10-13 07:40:54 +0300

    q4

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ testMultiplyArray(testArray);
 // Don't forget to create a new branch for your work on the next question, if you attempt it.
 
 /////////////////////////////////////
-/* STRETCH GOAL: Problem 6 
+/* STRETCH GOAL: Problem 6
 Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
